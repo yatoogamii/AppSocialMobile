04e85c46d2e4d5f8239e88b42697c993170c2966
yatoogamii <bracaval.elias.pro@gmail.com>
2020-04-21 23:52:24 +0200

    fix rotation of swiper and refactoring allCandidates with useState

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,152 +10,67 @@ import { db, FieldPath } from "../App";
 export function HomeScreen({ navigation }) {
   const appState = useContext(AppStateContext);
   const [lastCandidate, setLastCandidate] = useState(appState.userProfile.lastCandidate);
-  const [allUsers, setAllUsers] = useReducer(allUsersReducer, []);
+  const [allCandidates, setAllCandidates] = useState([]);
 
-  function allUsersReducer(state, action) {
-    if (action.name === "clean") {
-      return [];
-    }
+  async function refuseCandidate() {
+    try {
+      updateLastCandidate();
 
-    if (action.name === "init") {
-      return [...action.newValue];
-    } else {
-      const newState = state.slice();
-      setLastCandidate(newState[newState.length - 1].identity.userId);
-      // set lastCandidate into bdd
-      const userProfile = db
+      const newAllCandidates = Array.from(allCandidates);
+
+      // add userId to refuse list of currentUser
+      const newRefuseList = appState.userProfile.refuse;
+      newRefuseList.push(newAllCandidates[newAllCandidates.length - 1].identity.userId);
+      appState.setUserProfile({
+        refuse: newRefuseList,
+      });
+
+      // get current user
+      const userProfile = await db
         .collection("users")
         .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
         .get();
 
-      // @TODO use async
-      userProfile
-        .then(users => {
-          users.forEach(docs => {
-            docs.ref.update({
-              "match.lastCandidate": newState[newState.length - 1].identity.userId,
-            });
-          });
-        })
-        .catch(error => {
-          console.log(error);
+      // update match refuse list of current user into bdd
+      userProfile.forEach(docs => {
+        const newRefuseList = docs.data().match.refuse;
+        newRefuseList.push(newAllCandidates[newAllCandidates.length - 1].identity.userId);
+        docs.ref.update({
+          "match.refuse": newRefuseList,
         });
+      });
 
-      newState.pop();
+      // remove refused candidate
+      newAllCandidates.splice(newAllCandidates.length - 1, 1);
 
-      if (newState.length === 0) {
-        fetchAllProfiles();
+      if (newAllCandidates.length === 0) {
+        fetchAllProfiles(allCandidates[allCandidates.length - 1].identity.userId);
       }
 
-      if (action.name === "refuse") {
-        // add userId to refuse list of currentUser
-        const newRefuseList = appState.userProfile.refuse;
-        newRefuseList.push(state[state.length - 1].identity.userId);
-        appState.setUserProfile({
-          refuse: newRefuseList,
+      // update allCandidates without refused candidate
+      setAllCandidates(newAllCandidates);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function updateLastCandidate() {
+    try {
+      setLastCandidate(allCandidates[allCandidates.length - 1].identity.userId);
+
+      // set lastCandidate for current user into bdd
+      const userProfile = await db
+        .collection("users")
+        .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
+        .get();
+
+      userProfile.forEach(docs => {
+        docs.ref.update({
+          "match.lastCandidate": allCandidates[allCandidates.length - 1].identity.userId,
         });
-        const userProfile = db
-          .collection("users")
-          .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
-          .get();
-
-        // @TODO use async
-        userProfile
-          .then(users => {
-            users.forEach(docs => {
-              const newRefuseList = docs.data().match.refuse;
-              newRefuseList.push(state[state.length - 1].identity.userId);
-              docs.ref.update({
-                "match.refuse": newRefuseList,
-              });
-            });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }
-
-      if (action.name === "like") {
-        // if other user already like current user
-        if (state[state.length - 1].match.like.includes(appState.userProfile.userId)) {
-          // add other user into likeReciprocal of current user
-          const newLikeReciprocalList = appState.userProfile.likeReciprocal;
-          newLikeReciprocalList.push(state[state.length - 1].identity.userId);
-          appState.setUserProfile({
-            likeReciprocal: newLikeReciprocalList,
-          });
-          // update current user profile into bdd
-          const currentUserProfile = db
-            .collection("users")
-            .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
-            .get();
-
-          // @TODO use async
-          currentUserProfile
-            .then(users => {
-              users.forEach(docs => {
-                const newLikeReciprocalList = docs.data().match.likeReciprocal;
-                newLikeReciprocalList.push(state[state.length - 1].identity.userId);
-                docs.ref.update({
-                  "match.likeReciprocal": newLikeReciprocalList,
-                });
-              });
-            })
-            .catch(error => {
-              console.log(error);
-            });
-          // remove current user to like list of other user and add it to likeReciprocal too
-          const otherUserProfile = db
-            .collection("users")
-            .where(new FieldPath("identity", "userId"), "==", state[state.length - 1].identity.userId)
-            .get();
-
-          // @TODO use async
-          otherUserProfile
-            .then(users => {
-              users.forEach(docs => {
-                const newLikeReciprocalList = docs.data().match.likeReciprocal;
-                newLikeReciprocalList.push(appState.userProfile.userId);
-                const newLikeList = docs.data().match.like;
-                newLikeList.splice(newLikeList.indexOf(appState.userProfile.userId), 1);
-                docs.ref.update({
-                  "match.likeReciprocal": newLikeReciprocalList,
-                  "match.like": newLikeList,
-                });
-              });
-            })
-            .catch(error => {
-              console.log(error);
-            });
-        } else {
-          const newLikeList = appState.userProfile.like;
-          newLikeList.push(state[state.length - 1].identity.userId);
-          appState.setUserProfile({
-            like: newLikeList,
-          });
-          const userProfile = db
-            .collection("users")
-            .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
-            .get();
-
-          // @TODO use async
-          userProfile
-            .then(users => {
-              users.forEach(docs => {
-                const newLikeList = docs.data().match.like;
-                newLikeList.push(state[state.length - 1].identity.userId);
-                docs.ref.update({
-                  "match.like": newLikeList,
-                });
-              });
-            })
-            .catch(error => {
-              console.log(error);
-            });
-        }
-      }
-
-      return newState;
+      });
+    } catch (e) {
+      console.log(e);
     }
   }
 
@@ -168,37 +83,29 @@ export function HomeScreen({ navigation }) {
     }
   }
 
-  async function fetchAllProfiles() {
-    // gender
-    // sexual
-    // age
-    // city
-    // country
-    // hobbies
-    if (lastCandidate) {
-      console.log("last candidate true");
+  async function fetchAllProfiles(newLastCandidate = "") {
+    if (newLastCandidate !== "") {
       const query = await db
         .collection("users")
         .orderBy(new FieldPath("identity", "userId"))
-        .startAt(lastCandidate)
+        .startAfter(newLastCandidate)
         // .where(new FieldPath("identity", "gender"), "==", appState.userProfile.whatIWant.gender)
         // .where(new FieldPath("identity", "sexualOrientation"), "==", appState.userProfile.whatIWant.sexualOrientation)
         // .where(new FieldPath("identity", "city"), "==", appState.userProfile.whatIWant.city)
         .limit(5)
         .get();
 
-      const newAllUsers = [];
+      const newAllCandidates = [];
 
       query.forEach(user => {
         const userData = user.data();
 
         if (!appState.userProfile.refuse.includes(userData.identity.userId) && !appState.userProfile.like.includes(userData.identity.userId) && !appState.userProfile.likeReciprocal.includes(userData.identity.userId)) {
-          newAllUsers.push(userData);
+          newAllCandidates.push(userData);
         }
       });
-      setAllUsers({ name: "init", newValue: newAllUsers.reverse() });
+      setAllCandidates(newAllCandidates.reverse());
     } else {
-      console.log("last candidate false");
       const query = await db
         .collection("users")
         .orderBy(new FieldPath("identity", "userId"))
@@ -208,36 +115,39 @@ export function HomeScreen({ navigation }) {
         .limit(5)
         .get();
 
-      const newAllUsers = [];
+      const newAllCandidates = [];
 
       query.forEach(user => {
         const userData = user.data();
 
         if (!appState.userProfile.refuse.includes(userData.identity.userId) && !appState.userProfile.like.includes(userData.identity.userId) && !appState.userProfile.likeReciprocal.includes(userData.identity.userId)) {
-          newAllUsers.push(userData);
+          newAllCandidates.push(userData);
         }
       });
-      setAllUsers({ name: "init", newValue: newAllUsers.reverse() });
+      setAllCandidates(newAllCandidates.reverse());
     }
   }
 
   useEffect(() => {
     fetchAllProfiles();
-    return setAllUsers({ name: "clean" });
+    return () => {
+      setAllCandidates([]);
+    };
   }, []);
 
-  console.log("allUsers");
-  console.log(allUsers);
-  console.log("lastCandidate");
-  console.log(lastCandidate);
+  console.log(allCandidates);
 
   return (
     <>
       <View style={styles.containerCenter}>
-        <Image style={styles.image} source={{ uri: allUsers[0] ? allUsers[allUsers.length - 1].identity.photos[0] : "https://via.placeholder.com/150" }} />
-        <Text style={styles.title}>{allUsers[0] ? allUsers[allUsers.length - 1].identity.displayName : ""}</Text>
-        <Button title="Yes" onPress={() => setAllUsers({ name: "like" })} />
-        <Button title="No" onPress={() => setAllUsers({ name: "refuse" })} />
+        {allCandidates.length > 0 && (
+          <>
+            <Image style={styles.image} source={{ uri: allCandidates[allCandidates.length - 1].identity.photos[0] }} />
+            <Text style={styles.title}>{allCandidates[0] ? allCandidates[allCandidates.length - 1].identity.displayName : ""}</Text>
+            <Button title="Yes" onPress={() => {}} />
+            <Button title="No" onPress={() => refuseCandidate()} />
+          </>
+        )}
       </View>
       <View style={styles.containerCenter}>
         <Text style={styles.title}>Hello ! Bienvenue dans le Home</Text>
@@ -264,3 +174,151 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 });
+
+// if (action.name === "clean") {
+//   return [];
+// }
+
+// if (action.name === "init") {
+//   return [...action.newValue];
+// } else {
+//   const newState = state.slice();
+//   console.log("start of else function");
+//   console.log(newState);
+//   setLastCandidate(newState[newState.length - 1].identity.userId);
+//   // set lastCandidate into bdd
+//   const userProfile = db
+//     .collection("users")
+//     .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
+//     .get();
+
+//   // @TODO use async
+//   userProfile
+//     .then(users => {
+//       users.forEach(docs => {
+//         console.log("into then");
+//         console.log(newState);
+//         docs.ref.update({
+//           "match.lastCandidate": newState[newState.length - 1].identity.userId,
+//         });
+//       });
+//       newState.pop();
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+
+//   if (newState.length === 0) {
+//     fetchAllProfiles();
+//   }
+
+//   if (action.name === "refuse") {
+//     // add userId to refuse list of currentUser
+//     const newRefuseList = appState.userProfile.refuse;
+//     newRefuseList.push(state[state.length - 1].identity.userId);
+//     appState.setUserProfile({
+//       refuse: newRefuseList,
+//     });
+//     const userProfile = db
+//       .collection("users")
+//       .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
+//       .get();
+
+//     // @TODO use async
+//     userProfile
+//       .then(users => {
+//         users.forEach(docs => {
+//           const newRefuseList = docs.data().match.refuse;
+//           newRefuseList.push(state[state.length - 1].identity.userId);
+//           docs.ref.update({
+//             "match.refuse": newRefuseList,
+//           });
+//         });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+
+//   if (action.name === "like") {
+//     // if other user already like current user
+//     if (state[state.length - 1].match.like.includes(appState.userProfile.userId)) {
+//       // add other user into likeReciprocal of current user
+//       const newLikeReciprocalList = appState.userProfile.likeReciprocal;
+//       newLikeReciprocalList.push(state[state.length - 1].identity.userId);
+//       appState.setUserProfile({
+//         likeReciprocal: newLikeReciprocalList,
+//       });
+//       // update current user profile into bdd
+//       const currentUserProfile = db
+//         .collection("users")
+//         .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
+//         .get();
+
+//       // @TODO use async
+//       currentUserProfile
+//         .then(users => {
+//           users.forEach(docs => {
+//             const newLikeReciprocalList = docs.data().match.likeReciprocal;
+//             newLikeReciprocalList.push(state[state.length - 1].identity.userId);
+//             docs.ref.update({
+//               "match.likeReciprocal": newLikeReciprocalList,
+//             });
+//           });
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         });
+//       // remove current user to like list of other user and add it to likeReciprocal too
+//       const otherUserProfile = db
+//         .collection("users")
+//         .where(new FieldPath("identity", "userId"), "==", state[state.length - 1].identity.userId)
+//         .get();
+
+//       // @TODO use async
+//       otherUserProfile
+//         .then(users => {
+//           users.forEach(docs => {
+//             const newLikeReciprocalList = docs.data().match.likeReciprocal;
+//             newLikeReciprocalList.push(appState.userProfile.userId);
+//             const newLikeList = docs.data().match.like;
+//             newLikeList.splice(newLikeList.indexOf(appState.userProfile.userId), 1);
+//             docs.ref.update({
+//               "match.likeReciprocal": newLikeReciprocalList,
+//               "match.like": newLikeList,
+//             });
+//           });
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         });
+//     } else {
+//       const newLikeList = appState.userProfile.like;
+//       newLikeList.push(state[state.length - 1].identity.userId);
+//       appState.setUserProfile({
+//         like: newLikeList,
+//       });
+//       const userProfile = db
+//         .collection("users")
+//         .where(new FieldPath("identity", "userId"), "==", appState.userProfile.userId)
+//         .get();
+
+//       // @TODO use async
+//       userProfile
+//         .then(users => {
+//           users.forEach(docs => {
+//             const newLikeList = docs.data().match.like;
+//             newLikeList.push(state[state.length - 1].identity.userId);
+//             docs.ref.update({
+//               "match.like": newLikeList,
+//             });
+//           });
+//         })
+//         .catch(error => {
+//           console.log(error);
+//         });
+//     }
+//   }
+
+//   return newState;
+// }
